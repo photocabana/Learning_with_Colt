7dd83058f8946cbbb4806a8b86329c470cbdd9c6
Rindy <rinnn16@hotmail.com>
2024-04-22 14:47:04 -0500

    Notes update

--- JavaScriptBasics/basicsCheatsheet.js
+++ JavaScriptBasics/basicsCheatsheet.js
@@ -6,6 +6,61 @@
 0.345345;
 
 // Math Operations - PEMDAS (Parentheses, Exponents, Multiplication and Division, Addition and Subtraction)
+// Addition
+1 + 1; // 2
+
+// Subtraction
+2 - 1; // 1
+
+// Multiplication
+2 * 2; // 4
+
+// Division
+6 / 3; // 2
+
+// Exponents
+2 ** 4; // 16
+
+// Modulo - Remainder operator
+8 % 5; // 3
+
+// Strings - Characters surrounded by quotes
+("Hello, World!");
+("Goodbye, World!");
+
+// Concatenation - Combining strings
+"Hello" + "World"; // "HelloWorld"
+
+// Escape Characters - Backslash to escape characters
+("Singin' in the rain");
+
+// String Methods
+// Length - Property that returns the number of characters in a string
+"Hello".length; // 5
+
+// Index - Access individual characters using bracket notation
+"Hello"[0]; // "H"
+
+// Upper and Lower Case
+"Hello".toUpperCase(); // "HELLO"
+"Hello".toLowerCase(); // "hello"
+
+// Trim - Removes white space from beginning and end of string
+"   Hello   ".trim(); // "Hello"
+
+// Template Literals - Strings with embedded expressions
+`Hello, ${name}`; // "Hello, Colt"
+
+// Null and Undefined - Values that represent the absence of a value
+null;
+undefined;
+
+// Variables - Named containers for storing data
+// Declaring variables with var:
+var numberOfFriends = 1;
+
+//Incrementing:
+numberOfFriends += 3; //numberOfFriends is now 4
 
 //Making variables with let:
 let numberOfFriends = 1;
